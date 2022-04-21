f98160ababedfb9a08b4e8f7104bec42f0b1ef4d
Hayden Young <hayden@hbjy.dev>
2022-04-21 13:10:06 +0100

    docs: add API documentation for express, fastify, typeorm modules

--- libs/typeorm/src/index.ts
+++ libs/typeorm/src/index.ts
@@ -2,6 +2,15 @@ import { DataSource, DataSourceOptions, EntityTarget } from 'typeorm';
 
 const dataSources: Record<string, DataSource> = {};
 
+/**
+ * Returns an instance of a TypeORM DataSource to the user, optionally taking a
+ * key.
+ * 
+ * @param key The named instance of a DataSource to return
+ * @param options Any DataSource options to provide when instantiating the
+ * DataSource.
+ * @returns An instance of a TypeORM DataSource
+ */
 export const useTypeOrm = (
   key = 'default',
   options?: DataSourceOptions,
@@ -17,6 +26,9 @@ export const useTypeOrm = (
   return dataSources[key];
 };
 
+/**
+ * Initializes all registered TypeORM DataSources.
+ */
 export const initializeTypeOrm = async () => {
   await Promise.all(
     Object.keys(dataSources).map((key) => {
@@ -25,6 +37,11 @@ export const initializeTypeOrm = async () => {
   );
 };
 
+/**
+ * Returns a TypeORM repository for the user, given an Entity.
+ * @param entity The Entity to return a TypeORM repository for.
+ * @returns A TypeORM repository.
+ */
 export const UseTypeOrmRepository =
   <Entity>(entity: EntityTarget<Entity>) =>
   (key = 'default') =>
